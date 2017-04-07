063dd9aef7d8dc17a2a168aefff9acb04a8d4495
Mayner <meiningzhu@163.com>
2017-04-07 14:22:41 +0800

    组件拆分及router的使用

--- sell/build/webpack.base.conf.js
+++ sell/build/webpack.base.conf.js
@@ -18,6 +18,7 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
+      'common': path.resolve(__dirname, '../src/common'),
       'components': path.resolve(__dirname, '../src/components')
     }
   },
--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -8,6 +8,6 @@
     export default {};
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
 
 </style>
