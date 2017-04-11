61737a6823d96af2ae517aa3d84ab50c7a345205
Mayner <meiningzhu@163.com>
2017-04-11 17:56:04 +0800

    新增header组件样式

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -13,7 +13,7 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div v-if="seller.supports" class="support">
-                    <span class="icon"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
@@ -28,10 +28,67 @@
             seller: {
                 type: Object
             }
+        },
+        created() {
+            this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
         }
     };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin"
+    .header
+        color: rgb(255, 255, 255)
+        background: rgba(7, 17, 27, 0.5)
+        .content-wrapper
+            padding: 24px 12px 18px 24px
+            font-size: 0
+            .avatar
+                display: inline-block
+                margin-right: 16px
+                vertical-align: top
+                img
+                    border-radius: 2px
+            .content
+                display: inline-block
+                .title
+                    font-weight: bold
+                    line-height: 18px
+                    .brand
+                        display: inline-block
+                        vertical-align: top
+                        width: 30px
+                        height: 18px
+                        bg-image('brand')
+                        background-size: 30px 18px
+                        background-repeat: no-repeat
+                    .name
+                        display: inline-block
+                        margin-left: 6px
+                        font-size: 16px
+                .description
+                    margin: 8px 0 10px
+                    font-size: 12px
+                .support
+                    .icon
+                        display: inline-block
+                        width: 12px
+                        height: 12px
+                        vertical-align: top
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
+                        background-size: 12px 12px
+                        background-repeat: no-repeat
+                    .text
+                        margin-left: 4px
+                        font-size: 10px
 
 </style>
