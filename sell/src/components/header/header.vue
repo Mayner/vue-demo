ee7bd4cfdaf5301da72dcae6c41c397c5e6af589
Mayner <meiningzhu@163.com>
2017-04-13 01:35:51 +0800

    新增header组件内容

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,8 +17,18 @@
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
+            <div v-if="seller.supports" class="support-count">
+                <span class="count">{{seller.supports.length}}个</span>
+                <i class="icon-keyboard_arrow_right"></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" alt="" width="100%" height="100%">
+        </div>
     </div>
 </template>
 
@@ -38,9 +48,11 @@
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin"
     .header
+        position: relative
         color: rgb(255, 255, 255)
         background: rgba(7, 17, 27, 0.5)
         .content-wrapper
+            position: relative
             padding: 24px 12px 18px 24px
             font-size: 0
             .avatar
@@ -69,6 +81,7 @@
                 .description
                     margin: 8px 0 10px
                     font-size: 12px
+                    line-height: 12px
                 .support
                     .icon
                         display: inline-block
@@ -90,5 +103,59 @@
                     .text
                         margin-left: 4px
                         font-size: 10px
+                        line-height: 12px
+            .support-count
+                position: absolute
+                height: 24px
+                line-height: 24px
+                padding: 0 8px
+                border-radius: 14px
+                background-color: rgba(0,0,0,0.2)
+                right: 12px
+                bottom: 14px
+                .count
+                    font-size: 10px
+                    line-height: 24px
+                    text-align: center
+                .icon-keyboard_arrow_right
+                    font-size: 10px
+                    margin-left: 2px
+        .bulletin-wrapper
+            position: relative
+            padding: 0 22px 0 12px
+            height: 28px
+            line-height: 28px
+            white-space: nowrap
+            overflow: hidden
+            text-overflow: ellipsis
+            background: rgba(7, 17, 27, 0.2)
+            .bulletin-title
+                display: inline-block
+                vertical-align: top
+                margin-top: 8px
+                width: 22px
+                height: 12px
+                font-size: 10px
+                bg-image('bulletin')
+                background-size: 22px 12px
+                background-repeat: no-repeat 
+            .bulletin-text
+                vertical-align: top
+                margin: 0 4px
+                font-size: 10px
+            .icon-keyboard_arrow_right
+                position: absolute
+                font-size: 10px
+                right: 12px
+                top: 9px
+        .background
+            position: absolute
+            top: 0
+            left: 0
+            width: 100%
+            height: 100%
+            z-index: -1
+            filter: blur(10px)
+            
 
 </style>
