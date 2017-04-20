314f51a643dd20c851e4f3c4a2fed039200e33d6
Mayner <meiningzhu@163.com>
2017-04-21 01:01:46 +0800

    增加商品列表右侧样式

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -14,9 +14,9 @@
                 <li v-for="item in goods" class="food-list">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li v-for="food in item.foods" class="food-item">
+                        <li v-for="food in item.foods" class="food-item border-1px">
                             <div class="icon">
-                                <img :src="food.icon">
+                                <img width="57" height="57" :src="food.icon">
                             </div>
                             <div class="content">
                                 <h2 class="name">{{food.name}}</h2>
@@ -58,7 +58,6 @@
                 respones = respones.body;
                 if (respones.errno === ERR_OK) {
                     this.goods = respones.data;
-                    console.log(this.goods);
                 }
             });
         }
@@ -110,4 +109,31 @@
                     font-size: 12px
         .foods-wrapper
             flex: 1
+            .title
+                padding-left: 14px
+                height: 26px
+                line-height: 26px
+                border-left: 2px solid #d9dde1
+                font-size: 12px
+                color: rgb(147, 153, 159)
+                background: #f3f5f7
+            .food-item
+                display: flex
+                margin: 18px
+                padding-bottom: 18px
+                border-1px(rgba(7, 17, 27, 0.1))
+                &:last-child
+                    border-none()
+                    margin-bottom: 0
+                .icon
+                    flex: 0 0 57px
+                    margin-right: 10px
+                .content
+                    flex: 1
+                    .name
+                        margin: 2px 0 8px 0
+                        height: 14px
+                        line-height: 14px
+                        font-size: 14px
+                        color: rgb(7, 17, 27)
 </style>
