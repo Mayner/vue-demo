3a26077f8114606f98ec8688b5288be1a27a7a74
Mayner <meiningzhu@163.com>
2017-05-05 00:42:34 +0800

    添加左侧点击时的右侧联动功能

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -2,7 +2,7 @@
     <div class="goods">
         <div class="menu-wrapper" v-el:menu-wrapper>
             <ul>
-                <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex===$index}">
+                <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex===$index}" @click="selectMenu($index,$event)">
                     <span class="text border-1px">
                         <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
                     </span>
@@ -80,8 +80,18 @@
             });
         },
         methods: {
+            selectMenu(index, event) {
+                if (!event._constructed) {
+                    return;
+                }
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+                let el = foodList[index];
+                this.foodsScroll.scrollToElement(el, 300);
+            },
             _initScroll() {
-                this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+                this.menuScroll = new BScroll(this.$els.menuWrapper, {
+                    click: true
+                });
                 this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
                     probeType: 3
                 });
